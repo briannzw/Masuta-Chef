47bab413fc61dc9419c9cadb34d2f78f60c1cb2f
Brian Wijaya <70004754+briannzw@users.noreply.github.com>
2023-09-13 17:10:14 +0700

    Add Loot Lifetime

--- Assets/Scripts/Feature/Loot/LootObject.cs
+++ Assets/Scripts/Feature/Loot/LootObject.cs
@@ -6,6 +6,12 @@ namespace Loot.Interactable
 
     public class LootObject : MonoBehaviour, IInteractable
     {
+        [SerializeField] private float Lifetime = 20f;
+        private void Start()
+        {
+            Destroy(gameObject, Lifetime);
+        }
+
         public void Interact(GameObject other = null)
         {
             // TODO: other.WeaponSlot Exchange
