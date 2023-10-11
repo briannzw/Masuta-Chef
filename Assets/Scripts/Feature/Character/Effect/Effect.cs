15715ca5a0c708c002ea5d800273500aed0226a4
RosalenaBrigita <ocalimbong@gmail.com>
2023-10-11 13:30:13 +0700

    MedkitLoot

--- Assets/Scripts/Feature/Character/Effect/Effect.cs
+++ Assets/Scripts/Feature/Character/Effect/Effect.cs
@@ -5,21 +5,41 @@ using UnityEngine;
 namespace Character.StatEffect
 {
     using Kryz.CharacterStats;
+    using MyBox;
+
     public enum EffectBehaviour { Instant, Duration }
 
+    [System.Serializable]
     public class Effect
     {
         [Header("Values")]
         public StatsEnum StatsAffected;
         //public WeaponStatsEnum WeaponStatsAffected;
+        [SerializeField] protected float StatValue;
+        [SerializeField] protected StatModType StatModifierType;
         public StatModifier Modifier;
+
+        [Header("Advanced")]
+        [SerializeField] protected bool overwriteOrder;
+        [SerializeField, ConditionalField(nameof(overwriteOrder))] protected int priorityOrder;
 
         [Header("Type")]
         public EffectBehaviour Behaviour;
         public StatusEffects StatusEffect;
 
-        [Header("Time")]
-        public float Duration;
-        public float Interval;
+        [Space]
+        [ReadOnly, ConditionalField(nameof(Behaviour), false, EffectBehaviour.Duration)] public float Timer = 0f;
+        [ConditionalField(nameof(Behaviour), false, EffectBehaviour.Duration)] public float Duration;
+        [ConditionalField(nameof(Behaviour), false, EffectBehaviour.Duration)] public bool UseInterval;
+        [ConditionalField(new[] { nameof(Behaviour), nameof(UseInterval) }, new[] { false, false }, EffectBehaviour.Duration)][ReadOnly] public float IntervalTimer = 0f;
+        [ConditionalField(new[] { nameof(Behaviour), nameof(UseInterval) }, new[] { false, false }, EffectBehaviour.Duration)] public float Interval;
+
+        public bool DurationEnds => Timer >= Duration;
+
+        public void Initialize()
+        {
+            if (overwriteOrder) Modifier = new StatModifier(((StatusEffect == StatusEffects.Debuff) ? -1 : 1) * StatValue, StatModifierType, priorityOrder, this);
+            else Modifier = new StatModifier(((StatusEffect == StatusEffects.Debuff) ? -1 : 1) * StatValue, StatModifierType, this);
+        }
     }
 }
---
+++ Assets/Scripts/Feature/Loot/Medkit/MedkitLootObject.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Loot.Interactable
+{
+    using Character;
+    using Character.StatEffect;
+    using System.Collections.Generic;
+
+    public class MedkitLootObject : LootInteractObject, IApplyEffect
+    {
+        [field: SerializeField] public List<Effect> Effects { get; set; }
+
+        public void ApplyEffect(Character character)
+        {
+            foreach (Effect effect in Effects)
+            {
+                character.AddEffect(effect);
+            }
+        }
+
+        public override void Interact(GameObject other = null)
+        {
+            // Add player health
+            if (other.CompareTag("Player"))
+            {
+                ApplyEffect(other.GetComponent<Character>());
+            }
+        }
+    }
+}
