079152a4a9ea89c7f9dfd405f81b0bb5b7b6e85f
RiskaYurianda30 <142096871+RiskaYurianda30@users.noreply.github.com>
2023-10-31 09:56:05 +0700

    merge

---
+++ Assets/Scripts/Feature/Character/Hit/ContinuousAOEHit.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+namespace Character.Hit
+{
+    using Module.Detector;
+
+    public class ContinuousAOEHit : AOEController
+    {
+        [Header("Detection Settings")]
+        [SerializeField] private float areaRadius = 5f;
+        [SerializeField] private Vector3 areaOffset;
+        [SerializeField] private LayerMask targetMask;
+        [SerializeField] private float areaAngle = 120f;
+
+        [Header("Parameters")]
+        public float Delay = 0;
+        public float Interval = .5f;
+
+        private void OnEnable()
+        {
+            InvokeRepeating("DoHit", Delay, Interval);
+        }
+
+        private void OnDisable()
+        {
+            CancelInvoke();
+        }
+
+        private void DoHit()
+        {
+            characterInArea = ColliderDetector.Find<Character>(transform.position + areaOffset, areaRadius, targetMask, transform.forward, areaAngle);
+            foreach(Character character in characterInArea)
+            {
+                // Use AOEController HitChara
+                HitChara(character, true);
+            }
+        }
+
+#if UNITY_EDITOR
+        protected void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position + areaOffset, areaRadius);
+            Gizmos.color = Color.yellow;
+            GizmosExtensions.DrawWireArc(transform.position + areaOffset, transform.forward, areaAngle, areaRadius);
+        }
+#endif
+    }
+}
---
+++ Assets/Scripts/Feature/GameManager.cs
@@ -0,0 +1,46 @@
+using Level;
+using UnityEngine;
+using Save;
+using MyBox;
+using Character;
+using System;
+
+public class GameManager : MonoBehaviour
+{
+    #region Singleton
+    public static GameManager Instance { get; private set; }
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else Instance = this;
+
+        if(LoadOnAwake) LoadGame();
+    }
+    #endregion
+
+    [Header("Settings")]
+    public bool LoadOnAwake;
+
+    [Header("Static References")]
+    public Transform PlayerTransform;
+    public LevelManager LevelManager;
+    public SaveManager SaveManager;
+    public StatsManager StatsManager;
+
+    [ButtonMethod]
+    public void SaveGame()
+    {
+        SaveManager.Save();
+    }
+
+    [ButtonMethod]
+    public void LoadGame()
+    {
+        SaveManager.Load();
+        // Load Recipe Book Stat Mods
+        if(Application.isPlaying) StatsManager.Load();
+    }
+}
