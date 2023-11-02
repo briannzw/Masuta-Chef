c9f4a40a1a96f82c5a535db816794a05e268e66c
Rin <flowsnzr@gmail.com>
2023-11-02 19:20:29 +0800

    fix gun punya companion ngambang

---
+++ Assets/Scripts/Feature/NPC/Weapon/RangedController.cs
@@ -0,0 +1,49 @@
+using Character;
+using Character.Hit;
+using Spawner;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace NPC.NPCWeapon
+{
+    using Weapon;
+    using Character;
+    using Character.Hit;
+    public class RangedController : Weapon
+    {
+        #region Properties
+        public GameObject fireObjectPrefab;
+        #endregion
+
+        private new void Awake()
+        {
+            base.Awake();
+            rb = GetComponent<Rigidbody>();
+            if (rb == null) rb = GetComponentInParent<Rigidbody>();
+            Holder = GetComponentInParent<Character>();
+        }
+
+        protected new void Update()
+        {
+            base.Update();
+        }
+
+        #region Method
+        public override void Attack()
+        {
+            base.Attack();
+            var fireObject = Instantiate(fireObjectPrefab, transform.position, transform.rotation);
+            var controller = fireObject.GetComponent<BulletHit>();
+            controller.Initialize(this);
+            fireObject.GetComponent<Rigidbody>().velocity = transform.forward * fireObject.GetComponent<Bullet>().TravelSpeed;
+        }
+
+        public override void StartAttack()
+        {
+            base.StartAttack();
+        }
+        #endregion
+    }
+}
