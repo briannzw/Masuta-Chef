67129ba80b55a2f9c3efa27965a193e1cd9b6671
Rin <flowsnzr@gmail.com>
2023-11-29 15:04:56 +0800

    pasta gun ultimate bug

--- Assets/Scripts/Feature/Weapon/ExplodingBullet.cs
+++ Assets/Scripts/Feature/Weapon/ExplodingBullet.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Character.Hit;
 using UnityEngine;
+using Spawner;
 using Weapon;
 
 public class ExplodingBullet : Bullet
@@ -10,14 +11,44 @@ public class ExplodingBullet : Bullet
     private bool alreadyHit = false;
 
     [SerializeField] private float damageScaling;
+    private TrailRenderer trail;
 
     [Header("Explosion Properties")]
     public GameObject explosionApplicator;
     // Start is called before the first frame update
+
+    private void Awake()
+    {
+        trail = GetComponentInChildren<TrailRenderer>();
+    }
+
+    private void Update()
+    {
+        if (Vector3.Distance(startPosition, transform.position) >= Mathf.Abs(TravelDistance))
+        {
+            OnHit();
+            GetComponent<SpawnObject>().Release();
+        }
+    }
+
     protected void OnHit()
     {
         GameObject gameObject = Instantiate(explosionApplicator, transform.position, transform.rotation);
-        gameObject.GetComponentInChildren<AOEController>().Initialize(weapon, damageScaling);
+        gameObject.GetComponent<HitController>().Initialize(gameObject.GetComponent<BulletHit>().Source, damageScaling);
+    }
+
+    private void OnEnable()
+    {
+        GetComponent<Rigidbody>().velocity = transform.forward * TravelSpeed;
+    }
+
+    private void OnDisable()
+    {
+        GetComponent<Rigidbody>().velocity = Vector3.zero;
+        if (trail != null)
+        {
+            trail.Clear();
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -27,16 +58,7 @@ public class ExplodingBullet : Bullet
             // Play Some Effect
             alreadyHit = true;
             OnHit();
-            Destroy(gameObject);
-        }
-    }
-
-    private void Update()
-    {
-        if (Vector3.Distance(startPosition, transform.position) >= Mathf.Abs(TravelDistance))
-        {
-            OnHit();
-            Destroy(gameObject);
+            GetComponent<SpawnObject>().Release();
         }
     }
 }
